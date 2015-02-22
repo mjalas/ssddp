412d2394de699475c75aea8b2b15a29f09de90a9
Mats Jalas <mats.jalas@gmail.com>
2015-02-22 15:05:30 +0200

    Improved constructor. Added create_node_from_message helper method

--- node/peer_node.py
+++ node/peer_node.py
@@ -1,12 +1,22 @@
 from node.node_address import Address
 from service.service_list import ServiceList
+from message.message import Message
+
 
 class PeerNode(object):
     """
 
     """
-    def __init__(self, name, address, timestamp):
+    def __init__(self, name, address, timestamp, services=None):
         self.name = name
         self.address = address
         self.timestamp = timestamp
-        self.service_list = ServiceList()
+        if services is None:
+            self.service_list = ServiceList()
+        else:
+            self.service_list = services
+
+    @staticmethod
+    def create_node_from_message(message):
+        node = PeerNode(message.node_name, message.address, message.timestamp, message.services)
+        return node
