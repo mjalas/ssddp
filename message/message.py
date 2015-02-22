f9f357cef07da49f982fea02adfc36515a3aa137
Mats Jalas <mats.jalas@gmail.com>
2015-02-22 15:06:25 +0200

    Added service_to_list method

--- message/message.py
+++ message/message.py
@@ -13,3 +13,22 @@ class Message(object):
 
     def add_service(self, new_service):
         self.services.add(new_service)
+
+    def services_to_list(self):
+        service_list = []
+        for service in self.services.services:
+            service_list.append(service.to_json())
+        return service_list
+
+    def to_json(self):
+        data = {'name': self.node_name, 'address': {'ip': self.address.ip, 'tcp_port': self.address.tcp_port,
+                                                    'udp_port': self.address.udp_port}, 'timestamp': self.timestamp,
+                'services': self.services_to_list()}
+        return data
+
+    @staticmethod
+    def to_object(json_string):
+        data = json.loads(json_string)
+        address = Address(data['address']['ip'], data['address']['udp_port'], data['address']['tcp_port'])
+        message = Message(data['name'], address, data['timestamp'])
+        return message
