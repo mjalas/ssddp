de30f0dd31ab035ab128b3d9881b0141defa4348
Mats Jalas <mats.jalas@gmail.com>
2015-02-23 00:02:53 +0200

    added base for discovery manager

---
+++ manager/discovery_manager.py
@@ -0,0 +1,11 @@
+from manager.manager import Manager
+
+class DiscoveryManager(Manager):
+    """
+
+    """
+    def handle_message(self):
+        pass
+
+    def send_message(self):
+        pass
