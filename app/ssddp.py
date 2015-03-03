2f6d6c9f5b611872abe3e4766e251064e65aaeaa
Mats Jalas <mats.jalas@gmail.com>
2015-03-03 20:24:03 +0200

    Updated main loop to use new classes and methods.

--- app/ssddp.py
+++ app/ssddp.py
@@ -5,6 +5,9 @@ from networking.socket import Socket
 from message.description_request_list import DescriptionRequestList
 from manager.description_manager import DescriptionManager
 from manager.discovery_manager import DiscoveryManager
+from manager.broadcast_manager import BroadcastManager
+from manager.discovery_receive_manager import DiscoveryReceiveManager
+from manager.description_handler import DescriptionHandler
 
 # UDP socket & TCP socket
 udp_socket = Socket("UDP")
@@ -18,12 +21,14 @@ description_request_list = DescriptionRequestList()
 
 # Initialize Managers
 discovery_manager = DiscoveryManager()
-broadcast_manager = DiscoveryManager()
 description_manager = DescriptionManager()
+broadcast_manager = BroadcastManager(discovery_manager, peer_list, udp_socket)
+discovery_handler = DiscoveryReceiveManager()
+description_handler = DescriptionHandler(description_manager)
 input_manager = None    # Todo
 
 # Start Discovery Loop
-broadcast_manager.start_broadcast(peer_list, udp_socket)    # Todo
+broadcast_manager.start_broadcast()    # Todo
 
 input_list = [udp_socket, tcp_socket, sys.stdin]
 output_list = [tcp_socket]
@@ -38,12 +43,12 @@ while True:
         if x == udp_socket:
             # UDP -> Discovery Manager
             # (Receiving a UDP Discovery packet)
-            discovery_manager.handle_message(udp_socket)
+            discovery_handler.handle_discovery(udp_socket)
 
         elif x == tcp_socket:
             # TCP -> Description Manager
             # (Receiving a TCP Description Request or Response)
-            description_manager.handle_message(tcp_socket, description_request_list)
+            description_handler.handle_description(tcp_socket, description_request_list)
 
         elif x == sys.stdin:
             # STDIN -> Input Manager
