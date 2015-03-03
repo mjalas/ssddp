eff56e03b02d3b23cf2d63ce8a7c0a54dda1c04c
Mats Jalas <mats.jalas@gmail.com>
2015-03-03 18:35:00 +0200

    Added base for description manager class

---
+++ manager/description_manager.py
@@ -0,0 +1,12 @@
+from manager.manager import Manager
+
+
+class DescriptionManager(Manager):
+    """
+
+    """
+    def handle_message(self, message):
+        pass
+
+    def send_message(self, address):
+        pass
