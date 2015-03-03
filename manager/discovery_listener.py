c2a2005b8bedf2f27bdaa271d5458e639e993b03
Mats Jalas <mats.jalas@gmail.com>
2015-03-04 00:43:31 +0200

    Made several large changes to the class.
    Changed DiscoveryListener to extend Thread class, so it can be
    runned in the background. implemented handle_discovery and
    handle_data methods.

--- manager/discovery_listener.py
+++ manager/discovery_listener.py
@@ -1,20 +1,35 @@
-from manager.discovery_message_handler import DiscoveryMessageManager
-from app.globals import BUFFER_SIZE
+import threading
+
+from networking.socket import Socket
+from message.message import Message
 
 
-class DiscoveryListener(object):
+class DiscoveryListener(threading.Thread):
     """
         Listens to incoming discovery messages and handles them
     """
-    def __init__(self, discovery_manager):
-        if not isinstance(discovery_manager, DiscoveryMessageManager):
-            raise RuntimeError
-        self.discovery_manager = discovery_manager
+    def __init__(self, data, address, message_queue):
+        self._target = self.handle_discovery
+        self.data = data
+        self.address = address
+        self.message_queue = message_queue
+        threading.Thread.__init__(self)
 
-    def handle_discovery(self, udp_socket):  # TODO: Implement method
+    def handle_discovery(self):  # TODO: Implement method
         """
         Handles the incoming discovery message.
+        Creates a message object from the data
         :return:
         """
-        udp_socket.socket.recv(BUFFER_SIZE)
-        pass
+        message = self.handle_data()
+        if message:
+            self.message_queue.put(message)
+
+    def handle_data(self):
+        message = None
+        if self.data:
+            message = Message.to_object(self.data)
+        return message
+
+    def run(self):
+        self._target()
