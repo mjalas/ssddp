ebfecbd7c03123b4986cee59e65cf6ce69014ca5
Mats Jalas <mats.jalas@gmail.com>
2015-03-03 19:56:25 +0200

    added type check to send_message method

--- manager/description_manager.py
+++ manager/description_manager.py
@@ -1,12 +1,20 @@
 from manager.manager import Manager
 
+from message.message import Message, Address, ServiceList
+
 
 class DescriptionManager(Manager):
     """
     Subclass of Manager. Handles all description messages connected to a node.
     """
+    def __init__(self, node_name):
+        self.node_name = node_name;
+        self.sent_descriptions = []
+
     def handle_message(self, message):
-        pass
+        if isinstance(message, Message):
+            pass
 
     def send_message(self, address):
-        pass
+        if isinstance(address, Address):
+            message = Message(self.node_name)
