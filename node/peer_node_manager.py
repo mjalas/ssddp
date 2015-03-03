3fab122ff92e3fd0218bd21f6e6d38ca20854022
Mats Jalas <mats.jalas@gmail.com>
2015-03-04 00:45:25 +0200

    Added PeerNodeManager to handle updating of peer node data.

---
+++ node/peer_node_manager.py
@@ -0,0 +1,23 @@
+import threading
+
+from message.message import Message
+from node.peer_node_list import PeerNodeList
+
+
+class PeerNodeManager(threading.Thread):
+    """
+
+    """
+    def __init__(self, message_queue, node_list):
+        self.message_queue = message_queue
+        self.node_list = node_list
+        threading.Thread.__init__(self)
+        self.keep_alive = True
+
+    def run(self):
+        while self.keep_alive:
+            message = self.message_queue.get()
+            if isinstance(message, Message):
+                node = self.node_list.get(message.node_name)[0]
+                node.update_node(message)
+            self.message_queue.task_done()
