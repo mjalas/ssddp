0cc1b703044dcab5a8f2fd225c42b5776055cf25
Mats Jalas <mats.jalas@gmail.com>
2015-03-03 21:27:45 +0200

    Improved parameter names

--- manager/discovery_broadcast_loop.py
+++ manager/discovery_broadcast_loop.py
@@ -5,11 +5,11 @@ class DiscoveryBroadcastLoop(object):
     """
 
     """
-    def __init__(self, discovery_manager, peer_list, udp_socket):
-        if not isinstance(discovery_manager, DiscoveryMessageManager):
+    def __init__(self, discovery_message_manager, peer_list, udp_socket):
+        if not isinstance(discovery_message_manager, DiscoveryMessageManager):
             raise RuntimeError
-        self.discovery_manager = discovery_manager
-        self.latest_message = None
+        self.discovery_message_manager = discovery_message_manager
+        self.previous_message = None
         self.peer_list = peer_list
         self.udp_socket = udp_socket
 
@@ -22,7 +22,7 @@ class DiscoveryBroadcastLoop(object):
         if self.found_new_services():
             message = self.discovery_manager.create_message()
         self.send_message(message)
-        self.latest_message = message
+        self.previous_message = message
 
     def found_new_services(self):  # TODO: Implement real logic
         """
