ceebb869d3bfe7e72fa1699ab264cff915637912
Mats Jalas <mats.jalas@gmail.com>
2015-03-04 13:17:12 +0200

    Added usage of CommandListener

--- app/ssddp.py
+++ app/ssddp.py
@@ -13,73 +13,83 @@ from manager.discovery_message_handler import DiscoveryMessageHandler
 from manager.discovery_broadcast_loop import DiscoveryBroadcastLoop
 from manager.discovery_listener import DiscoveryListener
 from manager.description_listener import DescriptionListener
+from manager.command_listener import CommandListener
 from app.globals import TCP_LISTENING_PORT
 from app.globals import UDP_LISTENING_PORT
 from app.globals import BUFFER_SIZE
 
 
-def main():
-    # Handle program arguments
-    ArgumentHandler()
+class SSDDP(object):
 
-    # Self node
-    self_node = PeerNode()
+    def __init__(self):
+        pass
 
-    # Peer list
-    peer_list = PeerNodeList()
+    @staticmethod
+    def start():
+        # Handle program arguments
+        ArgumentHandler()
 
-    # Initialize Managers
-    discovery_manager = DiscoveryMessageHandler()
-    broadcast_manager = DiscoveryBroadcastLoop(discovery_manager, peer_list)
+        # Self node
+        self_node = PeerNode()
 
-    input_manager = None  # Todo
+        # Peer list
+        peer_list = PeerNodeList()
 
-    # Start Discovery Loop
-    broadcast_manager.start_broadcast()  # Todo
+        # Initialize Managers
+        discovery_manager = DiscoveryMessageHandler()
+        broadcast_manager = DiscoveryBroadcastLoop(discovery_manager, peer_list)
 
-    # Listening UDP and TCP socket setup
-    listening_udp_socket = Socket("UDP")
-    listening_udp_socket.bind(UDP_LISTENING_PORT)
+        input_manager = None  # Todo
 
-    listening_tcp_socket = Socket("TCP")
-    listening_tcp_socket.bind(TCP_LISTENING_PORT)
-    listening_tcp_socket.listen(5)
+        # Start Discovery Loop
+        broadcast_manager.start_broadcast()  # Todo
 
-    # Initialize message queue
-    message_queue = Queue()
+        # Listening UDP and TCP socket setup
+        listening_udp_socket = Socket("UDP")
+        listening_udp_socket.bind(UDP_LISTENING_PORT)
 
-    # Initialize manager that updates peer node data
-    peer_node_manager = PeerNodeManager(message_queue, peer_list)
-    peer_node_manager.run()
+        listening_tcp_socket = Socket("TCP")
+        listening_tcp_socket.bind(TCP_LISTENING_PORT)
+        listening_tcp_socket.listen(5)
 
-    input_list = [listening_udp_socket.socket, listening_tcp_socket.socket, sys.stdin]
+        # Initialize message queue
+        message_queue = Queue()
 
-    while True:
+        # Initialize manager that updates peer node data
+        peer_node_manager = PeerNodeManager(message_queue, peer_list)
+        peer_node_manager.run()
 
-        # listen (select UDP, TCP, STDIN)
-        input_ready, output_ready, except_ready = select.select(input_list, [], [])
+        input_list = [listening_udp_socket.socket, listening_tcp_socket.socket, sys.stdin]
 
-        for x in input_ready:
-
-            if x == listening_udp_socket.socket:
-                # UDP -> Discovery Manager
-                # (Receiving a UDP Discovery packet)
-                data, address = listening_udp_socket.recv(BUFFER_SIZE)
-                discovery_handler = DiscoveryListener(data, address, message_queue)
-                discovery_handler.run()
-
-            elif x == listening_tcp_socket.socket:
-                # TCP -> Description Manager
-                # (Receiving a TCP Description Request)
-                connection, client_address = listening_tcp_socket.accept()
-                description_handler = DescriptionListener(connection, client_address)
-                description_handler.run()
+        while True:
 
 
-            elif x == sys.stdin:  # TODO: handle user command (create new socket for sending messages and free it if required)
-                # STDIN -> Input Manager
-                input_manager.handle_message(sys.stdin)  # Todo
-                # TODO: output response to user inside thread!!
+            # listen (select UDP, TCP, STDIN)
+            input_ready, output_ready, except_ready = select.select(input_list, [], [])
+
+            for x in input_ready:
+
+
+                if x == listening_udp_socket.socket:
+                    # UDP -> Discovery Manager
+                    # (Receiving a UDP Discovery packet)
+                    data, address = listening_udp_socket.socket.recv(BUFFER_SIZE)
+                    discovery_handler = DiscoveryListener(data, address, message_queue)
+                    discovery_handler.run()
+
+                elif x == listening_tcp_socket.socket:
+                    # TCP -> Description Manager
+                    # (Receiving a TCP Description Request)
+                    connection, client_address = listening_tcp_socket.socket.accept()
+                    description_handler = DescriptionListener(connection, client_address)
+                    description_handler.run()
+
+                elif x == sys.stdin:  # TODO: handle user command (create new socket for sending messages and free it if required)
+                    # STDIN -> Input Manager
+                    command = sys.stdin.read(1024)
+                    input_listener = CommandListener(command)
+                    input_listener.run()
+                    # TODO: output response to user inside thread!!
 
 if __name__ == "__main__":
-    main()
+    SSDDP.start()
