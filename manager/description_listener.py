98135e8083b04914a4294b5603078a53c3bedf9d
Mats Jalas <mats.jalas@gmail.com>
2015-03-09 23:47:53 +0200

    Implemented missing parts.

--- manager/description_listener.py
+++ manager/description_listener.py
@@ -1,6 +1,14 @@
 from message.description_request_list import DescriptionRequestList
 import socket
 import threading
+from app.globals import BUFFER_SIZE
+from message.message import Message
+from message.message_types import MessageType
+from node.peer_node import PeerNode
+from message.timestamp import Timestamp
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class DescriptionListener(threading.Thread):
@@ -8,26 +16,36 @@ class DescriptionListener(threading.Thread):
         Manages descriptions.
         Handles incoming
     """
-    def __init__(self, connection, client_address):
+    def __init__(self, connection, client_address, node):
         if not isinstance(connection, socket.socket):
-            raise RuntimeError
+            raise IOError("Given connection not of type socket!")
+        if not isinstance(node, PeerNode):
+            raise ValueError("Node argument was not of type PeerNode!")
+        self.node = node
         self.connection = connection
         self.client_address = client_address
         self._target = self.respond_to_description_request
         threading.Thread.__init__(self)
 
-    def respond_to_description_request(self, description_request_list):
+    def respond_to_description_request(self):
         """
 
         :return:
         """
-        if not isinstance(description_request_list, DescriptionRequestList):
-            raise RuntimeError
-
-        # TODO: Finish response creation
-        response = ""
-        # TODO: Send response
-        self.connection.socket.send(response)
+        data = self.connection.recv(BUFFER_SIZE)
+        if len(data) <= 0:
+            return
+        try:
+            message = Message.to_object(data)
+        except ValueError as e:
+            log.error(e.args[0])
+            return
+        if message.message_type is MessageType.description_request:
+            response = Message(MessageType.description_response, self.node.name, self.node.address,
+                               Timestamp.create_timestamp(), self.node.service_list)
+            self.connection.socket.send(response.to_json())
+        else:  # Handle wrong request!
+            pass
 
     def run(self):
         self._target()
