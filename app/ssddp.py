3910434250920051348eb5b1e6f21b3ee06bdd44
Arto Suvitie <arto.suvitie@aalto.fi>
2015-03-10 20:06:54 +0200

    Pick random port for the sockets

--- app/ssddp.py
+++ app/ssddp.py
@@ -1,9 +1,12 @@
+import random
 import select
+import socket
 import sys
 from queue import Queue
 import logging
 
 from app.argument_handler import ArgumentHandler
+from app.globals import AVAILABLE_PORTS
 from app.logfile import Logfile
 from node.peer_node import PeerNode
 from node.peer_node_list import PeerNodeList
@@ -44,14 +47,32 @@ class SSDDP(object):
         discovery_manager = DiscoveryMessageHandler()
         broadcast_manager = DiscoveryBroadcastLoop(discovery_manager, peer_list)
 
-        input_manager = None  # Todo
-
         # Start Discovery Loop
-        broadcast_manager.start_broadcast()  # Todo
+        broadcast_manager.start_broadcast()
 
-        # Listening UDP and TCP socket setup
-        listening_udp_socket = Socket("UDP")
-        listening_tcp_socket = Socket("TCP")
+        while True:
+
+            # Listening UDP and TCP socket setup
+            listening_tcp_socket = Socket("TCP")
+            listening_udp_socket = Socket("UDP")
+
+            # Port
+            port = random.choice(AVAILABLE_PORTS)
+
+            try:
+                log.debug('Attempting to bind sockets to port %d', port)
+                listening_tcp_socket.bind(port)
+                listening_udp_socket.bind(port)
+                listening_tcp_socket.listen()
+
+            except socket.error as error:
+                log.error('Failed binding to port %d, (%s)', port, error)
+                listening_tcp_socket.terminate()
+                listening_udp_socket.terminate()
+                continue
+
+            log.info('Sockets bound to port %d', port)
+            break
 
         # Initialize message queue
         message_queue = Queue()
