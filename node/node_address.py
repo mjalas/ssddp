0e6675d966f4b76742f8b5c35bedd020d6a2429b
Mats Jalas <mats.jalas@gmail.com>
2015-03-10 20:08:33 +0200

    Change Address to only contain one port number.
    Replaced the separate TCP and UDP port numbers with a single port number.

--- node/node_address.py
+++ node/node_address.py
@@ -4,13 +4,9 @@ class Address(object):
     """
 
     """
-    def __init__(self, ip, udp_port, tcp_port):
+    def __init__(self, ip, port):
         self.ip = ip
-        self.udp_port = udp_port
-        self.tcp_port = tcp_port
+        self.port = port
 
-    def get_tcp_address(self):
-        return [self.ip, self.tcp_port]
-
-    def get_udp_address(self):
-        return [self.ip, self.udp_port]
+    def get_address(self):
+        return [self.ip, self.port]
