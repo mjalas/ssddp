70591fa056dd5e44d8641e02709cc8a18239cf75
Mats Jalas <mats.jalas@gmail.com>
2015-03-17 23:23:24 +0200

    Added structure for DescriptionListener tests

---
+++ manager/tests/test_description_listener.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest import mock
+
+
+class TestDescriptionListener(unittest.TestCase):
+    """
+    Test
+    """
+
+    def setUp(self):
+        pass
+
+    def test_respond_to_description_request(self):
+        pass
