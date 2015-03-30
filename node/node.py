af30b4112a3ba6982ef200987126d94d47791a26
Arto Suvitie <arto.suvitie@aalto.fi>
2015-03-30 19:48:48 +0300

    replace services with service_list_file

--- node/node.py
+++ node/node.py
@@ -3,11 +3,9 @@ from service.service_list import ServiceList
 
 class Node(object):
 
-    def __init__(self, name, address, services=None):
+    def __init__(self, name, address, service_list_file=None):
         self.name = name
         self.address = address
-        if services is None:
-            self.service_list = ServiceList()
-        else:
-            self.service_list = services
-
+        self.service_list = ServiceList()
+        if service_list_file:
+            self.service_list.read_file(service_list_file)
