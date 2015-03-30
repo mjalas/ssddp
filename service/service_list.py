60b7416bd70cd7d21fb5bfab7310f478d353e906
Arto Suvitie <arto.suvitie@aalto.fi>
2015-03-30 15:40:15 +0300

    Move function "to_list()" from message to service_list

--- service/service_list.py
+++ service/service_list.py
@@ -15,3 +15,12 @@ class ServiceList(object):
     def add(self, new_service):
         if isinstance(new_service, service):  # TODO: need to be tested -> create unit test
             self.services.append(new_service)
+
+    def to_list(self):
+        service_list = []
+        for a_service in self.services:
+            service_list.append(a_service.to_json())
+        return service_list
+
+    def __str__(self):
+        return self.to_list()
