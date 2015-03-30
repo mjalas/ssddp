c69db9e9c3cf2801064deac34b813d32b1823153
Arto Suvitie <arto.suvitie@aalto.fi>
2015-03-30 23:44:39 +0300

    describe problem

--- node/peer_node_list.py
+++ node/peer_node_list.py
@@ -29,6 +29,10 @@ class PeerNodeList(object):
             print("  #" + str(node_count) + ": \"" + str(node.name) + "\" (seen " + str(peer_node.timestamp)+")")
             for service in node.service_list:
                 print("   - SERVICE: " + service.name)
+                # TODO: AttributeError: 'dict' object has no attribute 'name'
+                # TODO: (happens only after receiving a couple of discovery messages)
+                # TODO: (probably peer list update changes service from object to dict)
+                # TODO: (OR the service should be dict but it works as object somehow first time)
                 if service.description:
                     print((4*' ') + service.description.replace('\n', '\n'+(4*' ')))
 
