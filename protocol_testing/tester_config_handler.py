0e737ae64c0c2220eaffd4c204a1f71095c9d975
Mats Jalas <mats.jalas@gmail.com>
2015-03-31 15:52:12 +0300

    Added feature to get configuration direct from json data.

--- protocol_testing/tester_config_handler.py
+++ protocol_testing/tester_config_handler.py
@@ -2,16 +2,28 @@ from protocol_testing.config_test_file import TestConfiguration
 from protocol_testing.config_test_file import ConfigurationNode
 
 
+class ArgumentError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
 class TesterConfigHandler(object):
     """
     Class for handling config files used for testing the protocol nodes.
     """
 
-    def __init__(self, file):
-        configuration = TestConfiguration.read_config_from_file(file)
-        if configuration is None:
-            raise FileNotFoundError
-        self.test_configuration = configuration
+    def __init__(self, file=None, data=None):
+        if not file and not data:
+            raise ArgumentError("Either file or data need to be present")
+        if file:
+            self.test_configuration = TestConfiguration.read_config_from_file(file)
+            if self.test_configuration is None:
+                raise FileNotFoundError
+        elif data:
+            self.test_configuration = TestConfiguration.read_config_from_data(data)
 
     def get_node_names(self):
         if self.test_configuration:
