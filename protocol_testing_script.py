d24d1d8536490b3bca07c114900337bd83d8a017
Mats Jalas <mats.jalas@gmail.com>
2015-03-31 15:52:56 +0300

    created simple example test script using ProtocolTester

---
+++ protocol_testing_script.py
@@ -0,0 +1,9 @@
+from protocol_testing.protocol_tester import ProtocolTester
+
+
+def main():
+    protocol_tester = ProtocolTester()
+    protocol_tester.start()
+
+if __name__ == '__main__':
+    main()
