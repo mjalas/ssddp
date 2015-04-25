365467ee990281437b5572f559d3773823ca2de1
Mats Jalas <mats.jalas@gmail.com>
2015-04-25 17:58:05 +0300

    Enabled usage of ConfigNodes in TesterConfigHandler

--- protocol_testing/tester_config_handler.py
+++ protocol_testing/tester_config_handler.py
@@ -15,15 +15,25 @@ class TesterConfigHandler(object):
     Class for handling config files used for testing the protocol nodes.
     """
 
-    def __init__(self, file=None, data=None):
+    def __init__(self, file=None, data=None, use_config_nodes=False):
         if not file and not data:
             raise ArgumentError("Either file or data need to be present")
-        if file:
-            self.test_configuration = TestConfiguration.read_config_from_file(file)
-            if self.test_configuration is None:
+        if use_config_nodes:
+            if file:
+                self.config_nodes = TestConfiguration.read_config_from_file_v2(file)
+            if self.config_nodes is None:
                 raise FileNotFoundError
-        elif data:
-            self.test_configuration = TestConfiguration.read_config_from_data(data)
+            elif data:
+                self.config_nodes = TestConfiguration.read_config_from_data_v2(data)
+            self.test_configuration = None
+        else:
+            if file:
+                self.test_configuration = TestConfiguration.read_config_from_file(file)
+                if self.test_configuration is None:
+                    raise FileNotFoundError
+            elif data:
+                self.test_configuration = TestConfiguration.read_config_from_data(data)
+            self.config_nodes = None
 
     def get_node_names(self):
         if self.test_configuration:
@@ -40,3 +50,4 @@ class TesterConfigHandler(object):
                     services[node.name] = node.services
             return services
         return None
+
