12645523370b7813a95abbdbd072d2afbbc02eef
Arto Suvitie <arto.suvitie@aalto.fi>
2015-05-14 19:56:05 +0300

    Add a function for generating a portscan message (aka. discovery request), which omits node service list.

--- message/discovery_message_handler.py
+++ message/discovery_message_handler.py
@@ -13,10 +13,26 @@ class DiscoveryMessageHandler(MessageHandler):
         pass
 
     def create_message(self, node):
-
+        """
+        Creates a standard discovery message from a node, includes service list
+        """
         message_type = MessageType.discovery_response
         timestamp = Timestamp.create_timestamp()
 
         message = Message(message_type, node.name, node.address, timestamp, node.service_list)
+
+        return message
+
+    def create_portscan_message(self, node):
+        """
+        Creates a simplified discovery message for port scanning purposes.
+        The node service list is omitted to reduce message size.
+
+        This message type is also known as "discovery request"
+        """
+        message_type = MessageType.discovery_request
+        timestamp = Timestamp.create_timestamp()
+
+        message = Message(message_type, node.name, node.address, timestamp, None)
 
         return message
