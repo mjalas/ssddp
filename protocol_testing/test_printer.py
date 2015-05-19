f7363a39db8374b144a367e80ebacc25845bfa73
Mats Jalas <mats.jalas@gmail.com>
2015-05-19 22:28:05 +0300

    small fixes in TestPrinter

--- protocol_testing/test_printer.py
+++ protocol_testing/test_printer.py
@@ -6,6 +6,7 @@ class TestPrinter(object):
     """
     def __init__(self, log_file="protocol_test.log"):
         self.log_file = log_file
+        self.log_messages = True
 
     def log(self, message, append=True):
         mode = 'w'
@@ -13,11 +14,11 @@ class TestPrinter(object):
             mode = 'a'
 
         with open(self.log_file, mode) as f:
-            tmp = str(datetime.now()) + ": " + message
+            tmp = str(datetime.now()) + ": " + message + "\n"
             f.write(tmp)
             f.flush()
 
-    def display(self, message, log=False):
-        if log:
+    def display(self, message):
+        if self.log_messages:
             self.log(message)
         print(message)
