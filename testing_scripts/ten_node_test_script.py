7334ecc21014ea9c9a72370fe83272abf153bed8
Mats Jalas <mats.jalas@gmail.com>
2015-05-20 23:02:36 +0300

    Added a ten node test scenario

---
+++ testing_scripts/ten_node_test_script.py
@@ -0,0 +1,38 @@
+import time
+
+from testing_scripts.testing_script_helpers import start_node
+from protocol_testing.protocol_tester import BaseProtocolTester
+from testing_scripts.log_file_handler import LogFileHandler
+
+base_log_file = "test_logs/ten_node_test"
+config_file = "test_configurations/ten_nodes.json"
+
+
+def main():
+    log_file = LogFileHandler.create_log_file(base_log_file)
+    tester = BaseProtocolTester(10, log_file, __file__)
+    try:
+        # Setup nodes for the test
+        tester.setup_nodes_from_config_file(config_file)
+        # Start nodes in turn and check measurements.
+        start_node(tester, 0, 0)
+        start_node(tester, 1, 1)
+        start_node(tester, 2, 2)
+        start_node(tester, 3, 3)
+        start_node(tester, 4, 4)
+        start_node(tester, 5, 5)
+        start_node(tester, 6, 6)
+        start_node(tester, 7, 7)
+        start_node(tester, 8, 8)
+        start_node(tester, 9, 9)
+
+        time.sleep(10)
+
+        tester.end_test(no_prompt=True)
+    except KeyboardInterrupt:
+        tester.end_test(no_prompt=True)
+
+if __name__ == '__main__':
+    main()
+
+
