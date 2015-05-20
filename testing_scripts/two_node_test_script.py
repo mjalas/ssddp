7b7cbf64794a9e57f0b75cb8e765414c37f89318
Mats Jalas <mats.jalas@gmail.com>
2015-05-20 22:49:51 +0300

    small fixes to two node test script

--- testing_scripts/two_node_test_script.py
+++ testing_scripts/two_node_test_script.py
@@ -1,6 +1,6 @@
-from message.timestamp import Timestamp
+import time
 
-from app.globals import NodeCommand
+
 from protocol_testing.protocol_tester import BaseProtocolTester
 from testing_scripts.log_file_handler import LogFileHandler
 
@@ -9,23 +9,23 @@ config_file = "test_configurations/two_nodes.json"
 
 
 def main():
-
     log_file = LogFileHandler.create_log_file(base_log_file)
-
     tester = BaseProtocolTester(2, log_file, __file__)
-    # Setup nodes for the test
-    tester.setup_nodes_from_config_file(config_file)
-    # Start nodes in turn and check measurements.
-    #sender_name = tester.get_random_remote_name()
-    node_count = 1
-    #tester.start_node(sender_name, node_count)
-    node_count += 1
+    try:
+        # Setup nodes for the test
+        tester.setup_nodes_from_config_file(config_file)
+        # Start nodes in turn and check measurements.
+        sender_name = tester.get_random_remote_name()
+        tester.start_node(sender_name, 0)
 
+        sender_name = tester.get_new_random_remote_name()
+        tester.start_node(sender_name, 1)
 
+        time.sleep(10)
 
-
-    tester.end_test(no_prompt=True)
-
+        tester.end_test(no_prompt=True)
+    except KeyboardInterrupt:
+        tester.end_test(no_prompt=True)
 
 if __name__ == '__main__':
     main()
