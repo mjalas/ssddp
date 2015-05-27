a3026aeac1d851d1d5cf1c50e83244f23ce05dc2
Mats Jalas <mats.jalas@gmail.com>
2015-05-27 22:03:56 +0300

    Moved BaseUIPrinter to own file

---
+++ printers/base_ui_printer.py
@@ -0,0 +1,20 @@
+from printers.test_printer import TestPrinter
+
+
+class BaseUIPrinter(object):
+    """
+    Base logger class
+    """
+    def __init__(self, log_file, display=True, debug=False, log_messages=False):
+        self.debug = debug
+        self.display_outputs = display
+        self.printer = TestPrinter(log_file)
+        self.log_messages = log_messages
+
+    def display(self, message):
+        if self.display_outputs:
+            self.printer.display(message)
+
+    def log(self, message, append=True):
+        self.printer.log(message, append)
+
