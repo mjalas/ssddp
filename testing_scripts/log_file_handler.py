5b1a59be0ed5725c2b02f67d49cc4e2538719df5
Mats Jalas <mats.jalas@gmail.com>
2015-05-27 22:04:26 +0300

    Small fixes

--- testing_scripts/log_file_handler.py
+++ testing_scripts/log_file_handler.py
@@ -5,11 +5,16 @@ from message.timestamp import Timestamp
 
 class LogFileHandler(object):
 
+
     @staticmethod
     def create_file_if_not_available(file_path):
         if not path.exists(file_path):
-            with open(file_path, 'a'):
-                utime(file_path, None)
+            try:
+                with open(file_path, 'w'):
+                    utime(file_path, None)
+            except FileNotFoundError:
+                print(__file__)
+
 
     @staticmethod
     def create_log_file(base_filename):
