f9f1a8531949c70d7d0f635bc5dfc07fa4bcee4f
Mats Jalas <mats.jalas@gmail.com>
2015-05-29 14:52:04 +0300

    Created BasePrinter

--- printers_and_loggers/printer.py
+++ printers_and_loggers/printer.py
@@ -2,10 +2,25 @@ from datetime import datetime
 from message.timestamp import Timestamp
 
 
-class Printer(object):
+class BasePrinter(object):
+    """
+    """
+    def __init__(self):
+        pass
+
+    def log(self, message):
+        self.display(message)
+
+    @staticmethod
+    def display(message):
+        print(message)
+
+
+class Printer(BasePrinter):
     """
     """
     def __init__(self, log_file="protocol_test.log", print_to_ui=False):
+        super().__init__()
         self.log_file = log_file
         self.print_to_ui = print_to_ui
         self.log_messages = True
@@ -18,11 +33,5 @@ class Printer(object):
             tmp = str(datetime.now()) + ": " + str(message) + "\n"
             f.write(tmp)
             f.flush()
-
         if self.print_to_ui:
-            self.display(message)
-
-    @staticmethod
-    def display(message):
-        print(message)
-
+            super().display(message)
